02528a635d4d988e516b9bf2f5752a199bfefc39
DoraLC <34760377+DoraLC@users.noreply.github.com>
2023-08-29 17:43:42 +0800

    updates

--- drone.ts
+++ drone.ts
@@ -12,7 +12,11 @@ enum movement{
     //%block="Backward"
     backward, 
     //%block="Stay"
-    stay
+    stay, 
+    //%block="Anti-Clockwise"
+    anticlockwise, 
+    //%block="Clockwise"
+    clockwise
 }
 
 //% block="Drone STEM@eClass" color=#2B4556 icon="\uf1d8"
@@ -21,41 +25,42 @@ namespace drone {
     serial.redirect(SerialPin.P2, SerialPin.P1, 115200)
 
     //initialize commands
-    let command: number[] = [170, 0, 0]
+    let command: string = "170"
     let took_off: boolean = false
     let moving: boolean = false
-    let fly_flag: number = 0
-    let direction: number = 0
+    let fly_flag: string = "00"
+    let direction: string = "00"
 
     //changing drone status
     basic.forever(function() {
-        command[1] = fly_flag
-        command[2] = direction
-        serial.writeNumbers(command)
+        command = "170"
+        command += fly_flag
+        command += direction
+        serial.writeString(command)
         basic.pause(100)
     })
 
     //%block="Be prepared" weight=100
     export function prepared(): void{
-        fly_flag = 1
-        direction = 0
+        fly_flag = "01"
+        direction = "00"
     }
 
     //%block="END" weight=99
     export function end_section(): void{
-        fly_flag = 0
-        direction = 0
+        fly_flag = "00"
+        direction = "00"
     }
 
     //%block="Take off" weight=98
     export function take_off(): void{
-        direction = 6
+        direction = "06"
         took_off = true
     }
 
     //%block="Land" weight=97
     export function land(): void{
-        direction = 7
+        direction = "07"
         took_off = false
     }
 
@@ -63,36 +68,44 @@ namespace drone {
     export function Action(action: movement): void {
         if (took_off){
             switch (action) {
+                case movement.clockwise:
+                    direction = "11"
+                    moving = true
+                    break
+                case movement.anticlockwise:
+                    direction = "12"
+                    moving = true
+                    break
                 case movement.up:
-                    direction = 1
+                    direction = "01"
                     moving = true
                     break
                 case movement.down:
-                    direction = 0
+                    direction = "00"
                     moving = true
                     break
                 case movement.right:
-                    direction = 4
+                    direction = "04"
                     moving = true
                     break
                 case movement.left:
-                    direction = 5
+                    direction = "05"
                     moving = true
                     break
                 case movement.forward:
-                    direction = 2
+                    direction = "02"
                     moving = true
                     break
                 case movement.backward:
-                    direction = 3
+                    direction = "03"
                     moving = true
                     break
                 case movement.stay:
                     if (moving){
-                        direction = 6
-                        command[2] = direction
-                        serial.writeNumbers(command)
-                        direction = 9
+                        direction = "06"
+                        command = "170" + fly_flag + direction
+                        serial.writeString(command)
+                        direction = "09"
                         moving = false
                     }
                     break
